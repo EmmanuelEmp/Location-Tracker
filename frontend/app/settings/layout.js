3c1d68d3c1bfe25035e8a1da3828babcfee90126
FIYINFOLUWA <ajayifiyin42@gmail.com>
2024-03-13 00:28:42 +0100

    settings page created

--- frontend/app/settings/layout.js
+++ frontend/app/settings/layout.js
@@ -10,7 +10,7 @@
 
 export default function SettingsLayout({ children }) {
   return (
-    <section>
+    <section className="settings">
       {children}
     </section>
   )
--- frontend/app/settings/page.js
+++ frontend/app/settings/page.js
@@ -8,6 +8,101 @@ import Link from 'next/link'
 export default function Settings() {
 
   return (
-    <p>Settings</p>
+    <>
+      <Image
+        src={"/assests/male.png"}
+        width={161.504}
+        height={161.504}
+        alt='profile picture'
+        className='profile-img'
+      />
+
+      <h2 className='profile-name'>Alex</h2>
+
+      <div className='profile-div'>
+
+        <h4 className='profile-header'>
+          Profile Info
+        </h4>
+
+        <div className='profile-info'>
+          <h3 className='profile-h3'>Name:</h3>
+
+          <p className='profile-details'>
+            Alex Meaida
+          </p>
+        </div>
+
+
+        <div className='profile-info'>
+          <h3 className='profile-h3'>Email:</h3>
+
+          <p className='profile-details'>
+            meadia@gmail.com
+          </p>
+        </div>
+
+
+        <div className='profile-info'>
+          <h3 className='profile-h3'>Age:</h3>
+
+          <p className='profile-details'>
+            20
+          </p>
+        </div>
+
+
+        <div className='profile-info'>
+          <h3 className='profile-h3'>Gender:</h3>
+
+          <p className='profile-details'>
+            Male
+          </p>
+        </div>
+
+
+        <div className='profile-info'>
+          <h3 className='profile-h3'>Address:</h3>
+
+          <p className='profile-details'>
+            Tokoyo, Japan
+          </p>
+        </div>
+
+        <div className='profile-info'>
+          <h3 className='profile-h3'>Phone:</h3>
+
+          <p className='profile-details'>
+            080 123 456 78
+          </p>
+        </div>
+
+        <div className='profile-info'>
+          <h3 className='profile-h3'>Country:</h3>
+
+          <p className='profile-details'>
+            Japan
+          </p>
+        </div>
+      </div>
+
+      <div className='profile-option-div'>
+        <div className='delete-div'>
+          <p className='delete-pgh'>Delete Account</p>
+
+          <Image
+            src={"/assests/delete.png"}
+            width={12}
+            height={12}
+            alt='profile picture'
+            className='profile-img'
+          />
+        </div>
+
+        <div className='edit-div'>
+          <p className='edit-pgh'>Edit Info</p>
+        </div>
+      </div>
+    </>
   )
 }
